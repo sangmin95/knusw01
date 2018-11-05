58adf401dc05840efb8051cd414021f17f8be9fe
sangmin <sangmin95@gmail.com>
2018-11-05 20:48:00 +0900

    receiver end

--- code/bluetooth_dc/bluetooth_dc.ino
+++ code/bluetooth_dc/bluetooth_dc.ino
@@ -2,6 +2,8 @@
 
 const int motorPin = 9;     //상수 선언 : 모터와 연결된 핀 번호 저장
 SoftwareSerial mySerial(2, 3); //블루투스의 Tx, Rx핀을 2번 3번핀으로 설정
+bool flag;
+int signal=-1;
 
 void setup() {
   // 시리얼 통신의 속도를 9600으로 설정
@@ -11,23 +13,21 @@ void setup() {
   while (!Serial) {
     ; //시리얼통신이 연결되지 않았다면 코드 실행을 멈추고 무한 반복
   }
-
-
-  Serial.println("Hello World!");
+  Serial.println("connected");
 
   //블루투스와 아두이노의 통신속도를 9600으로 설정
   mySerial.begin(9600);
 }
 
 void loop() { //코드를 무한반복합니다.
-  bool flag;
   
   if (mySerial.available()) { //블루투스에서 넘어온 데이터가 있다면
-    Serial.write(mySerial.read()); //시리얼모니터에 데이터를 출력
-    spiningMotor(flag);
-  }
-  if (Serial.available()) {    //시리얼모니터에 입력된 데이터가 있다면
-    mySerial.write(Serial.read());  //블루투스를 통해 입력된 데이터 전달
+    signal = mySerial.read();
+    Serial.write(signal); //시리얼모니터에 데이터를 출력
+    if(signal > 0)
+      flag = true;
+    else
+      flag = false;
     spiningMotor(flag);
   }
 }
