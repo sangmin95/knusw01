cbe13850a2fc06e5452c7c10f3979c1c0b076c53
sangmin <sangmin95@gmail.com>
2018-11-06 11:19:11 +0900

    co2code update

--- code/co2/co2.ino
+++ code/co2/co2.ino
@@ -1,10 +1,13 @@
 #include "CO2Sensor.h"
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); //블루투스의 Tx, Rx핀을 2번 3번핀으로 설정
 CO2Sensor co2Sensor(A0, 0.99, 100);
 
 void setup() {
 
   Serial.begin(9600);
-
+  mySerial.begin(9600);
   Serial.println("=== Initialized ===");
 
   co2Sensor.calibrate();
@@ -17,9 +20,14 @@ void loop() {
   Serial.print("CO2 value: ");
 
   Serial.println(val);
-
-
-
-  delay(1000);
+  if(mySerial.available() > 0){
+    Serial.println("mySerial available()");
+    if(val > 600){
+    Serial.println("turn on");
+    mySerial.write("1");
+    }
+  }
+  
+  delay(500);
 
 }
